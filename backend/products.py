74123bc79071d1f57becf4509fdaa4a19073de36
Huzaiiiifa <inothingie5@gmail.com>
2025-12-04 17:05:35 +0300

    made it ready

--- backend/products.py
+++ backend/products.py
@@ -1,5 +1,16 @@
-products = ["Sennheiser 600", "Sony WH-1000XM6", "Sony WH-1000XM4 Wireless", "Anker Soundcore Space Q45 Wireless", "Anker Soundcore Life Q20 2024", "Focal Bathys Wireless"]
-prices = [149, 199, 89, 129, 159, 79]
+products = ["Sennheiser 600", 
+            "Sony WH-1000XM6", 
+            "Sony WH-1000XM4 Wireless", 
+            "Anker Soundcore Space Q45 Wireless", 
+            "Anker Soundcore Life Q20 2024", 
+            "Focal Bathys Wireless"]
+
+prices = [149, 
+          199, 
+          89,
+          129,
+          159,
+          79]
 
 print("اختر رقم المنتج:")
 for x in range(len(products)):
